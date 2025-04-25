ca76f51c3f9453c861f774624fd093f71f1cd47d
gsparvej <parvejmd4469@gmail.com>
2025-04-25 21:55:26 +0600

    add

--- RandomMathGuessNumber/src/randommathguessnumber/RandomMathGuessNumber.java
+++ RandomMathGuessNumber/src/randommathguessnumber/RandomMathGuessNumber.java
@@ -10,19 +10,46 @@ public class RandomMathGuessNumber {
     public static void main(String[] args) {
         
         Scanner s=new Scanner(System.in);
-        System.out.print("Guess a Number : ");
-        int a=s.nextInt();
+        int count=0;
+        boolean status=false;
         int random=(int) (Math.random()*10);
         System.out.println("Random Number : "+random);
         
-        if(a==random){
+        
+        
+        while(count <3 ){
+        
+            System.out.println("Enter Your Guess : ");
+            int guess=s.nextInt();
+            
+         if(guess==random){
         
             System.out.println("You Are Win ");
+           status=true;
+           break;
         }
-        else{
+        else if(!(guess==random) ){
         
-            System.out.println("Better Luck Next Time ");
+            System.out.println("Try Again");
+          
+            status=false;
+            
+        
         }
+//        else{
+//        
+//            System.out.println("Better Luck Next Time ");
+//            
+//        }
+         count++;
+        
+       
+        }
+        if (!false) {
+            System.out.println("Better Luck Next Time! The correct number was: " + random);
+        }
+        
+       
         
     }
     
