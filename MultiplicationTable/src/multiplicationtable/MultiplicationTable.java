facf347e37611bb53fa9d2be7c986d030f75005d
gsparvej <parvejmd4469@gmail.com>
2025-04-25 18:53:51 +0600

    add

--- MultiplicationTable/src/multiplicationtable/MultiplicationTable.java
+++ MultiplicationTable/src/multiplicationtable/MultiplicationTable.java
@@ -1,12 +1,25 @@
 
 package multiplicationtable;
 
+import java.util.Scanner;
+
 
 public class MultiplicationTable {
 
     
     public static void main(String[] args) {
         
+        Scanner s=new Scanner(System.in);
+        System.out.println("Which Multiplication Table You Want To See");
+        int input =s.nextInt();
+        int mul=1;
+        
+        for(int i=1;i<=10;i++){
+        
+            mul*=i;
+            System.out.println(input+"X"+i+"="+mul);
+           // System.out.println(input+"X"+i+"="+mul);
+        }
     }
     
 }
