3e0e4de27c41ab380aefdb3ae8aa21a42fe2e763
gsparvej <parvejmd4469@gmail.com>
2025-04-25 18:24:45 +0600

    changed

--- OneToInputNumberOddPrint/src/onetoinputnumberoddprint/OneToInputNumberOddPrint.java
+++ OneToInputNumberOddPrint/src/onetoinputnumberoddprint/OneToInputNumberOddPrint.java
@@ -1,13 +1,27 @@
 
 package onetoinputnumberoddprint;
 
+import java.util.Scanner;
+
 
 public class OneToInputNumberOddPrint {
 
    
     public static void main(String[] args) {
         
+        Scanner s=new Scanner(System.in);
+        System.out.print("How much you want to see Odd Number : ");
+        int input=s.nextInt();
         
+        for(int i=1;i<input;i++){
+        
+            if(i%2==1){
+            
+                System.out.print(+i+" ");
+            
+            }
+        
+        }
     }
     
 }
