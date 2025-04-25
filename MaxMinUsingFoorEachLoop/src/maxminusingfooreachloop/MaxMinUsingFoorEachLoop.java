364452ca7e225d6c74e9e85cba63d2fb7f1de34a
gsparvej <parvejmd4469@gmail.com>
2025-04-25 12:33:06 +0600

    changed

--- MaxMinUsingFoorEachLoop/src/maxminusingfooreachloop/MaxMinUsingFoorEachLoop.java
+++ MaxMinUsingFoorEachLoop/src/maxminusingfooreachloop/MaxMinUsingFoorEachLoop.java
@@ -1,72 +1,63 @@
-
 package maxminusingfooreachloop;
 
 import java.util.Arrays;
 import java.util.Scanner;
 
-
 public class MaxMinUsingFoorEachLoop {
 
-    
     public static void main(String[] args) {
-        
-        Scanner s=new Scanner(System.in);
+
+        Scanner s = new Scanner(System.in);
         System.out.println("Enter Array size");
-        int input=s.nextInt();
-        
+        int input = s.nextInt();
+
         String result;
-        int maxNumber=0;
-        int minNumber=999999999;
-        
-        int[] myNumbers=new int[input];
-        
-        for(int index=0;index<input;index++){
-        
-            System.out.println("Input "+(index+1)+position(index+1));
-            int userInput=s.nextInt();
-            myNumbers[index]=userInput;
+        int maxNumber = 0;
+        int minNumber = 999999999;
+
+        int[] myNumbers = new int[input];
+
+        for (int index = 0; index < input; index++) {
+
+            System.out.println("Input " + (index + 1) + position(index + 1));
+            int userInput = s.nextInt();
+            myNumbers[index] = userInput;
         }
-        int index = 0;
-        
-        
-       
-        System.out.println("My Numbers :"+Arrays.toString(myNumbers));
-        
-        for(int number : myNumbers){
-        
-            if(number>maxNumber){
-            
-            maxNumber=number;
+
+        System.out.println("My Numbers :" + Arrays.toString(myNumbers));
+
+        for (int number : myNumbers) {
+
+            if (number > maxNumber) {
+
+                maxNumber = number;
             }
         }
-        for(int number: myNumbers){
-        
-            if(number<minNumber){
-            minNumber=number;
-            
+        for (int number : myNumbers) {
+
+            if (number < minNumber) {
+                minNumber = number;
+
             }
         }
-        System.out.println("Max Number is :"+maxNumber);
-        System.out.println("Min Number is :"+minNumber);
-        
+        System.out.println("Max Number is :" + maxNumber);
+        System.out.println("Min Number is :" + minNumber);
+
     }
-    
-    public static String position(int number){
-    
-         switch (number) {
-            case 1: 
-            return "st";
-                case 2: 
-             return "nd";
-                
-                case 3: 
-             return"rd";
-                
-                
+
+    public static String position(int number) {
+
+        switch (number) {
+            case 1:
+                return "st";
+            case 2:
+                return "nd";
+            case 3:
+                return "rd";
             default:
-                return"th";
+                return "th";
         }
-        
+
     }
-    
+
 }
