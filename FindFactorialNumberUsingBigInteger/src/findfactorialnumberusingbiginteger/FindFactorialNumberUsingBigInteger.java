38ee0a36da930412489aa987a852d2fea0bf548f
gsparvej <parvejmd4469@gmail.com>
2025-04-25 23:04:25 +0600

    add

--- FindFactorialNumberUsingBigInteger/src/findfactorialnumberusingbiginteger/FindFactorialNumberUsingBigInteger.java
+++ FindFactorialNumberUsingBigInteger/src/findfactorialnumberusingbiginteger/FindFactorialNumberUsingBigInteger.java
@@ -1,13 +1,26 @@
 
 package findfactorialnumberusingbiginteger;
 
+import java.math.BigInteger;
+import java.util.Scanner;
+
 
 public class FindFactorialNumberUsingBigInteger {
 
    
     public static void main(String[] args) {
        
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter value ");
+       int input=s.nextInt();
+        BigInteger factorial=BigInteger.ONE;
         
+        for(int i=1;i<=input;i++){
+        
+            factorial=factorial.multiply(BigInteger.valueOf(i));
+        
+        }
+        System.out.println("The Factorial of "+input+" is : "+factorial);
     }
     
 }
