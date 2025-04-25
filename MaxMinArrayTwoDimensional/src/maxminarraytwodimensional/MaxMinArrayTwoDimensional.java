41a693fe75908977c9a0397cd9f10e6e47db4956
gsparvej <parvejmd4469@gmail.com>
2025-04-25 11:19:25 +0600

    changed

--- MaxMinArrayTwoDimensional/src/maxminarraytwodimensional/MaxMinArrayTwoDimensional.java
+++ MaxMinArrayTwoDimensional/src/maxminarraytwodimensional/MaxMinArrayTwoDimensional.java
@@ -15,11 +15,25 @@ public class MaxMinArrayTwoDimensional {
         int rows=s.nextInt();
         System.out.println("Enter array col size");
         int cols=s.nextInt();
-        
-        
+       
         int[][] myArray=new int[rows][cols];
         
         System.out.println("Arrays : "+Arrays.deepToString(myArray));
+        System.out.println("------------------------");
+        
+        for(int row=0;row<rows;row++){
+        
+            for(int col=0;col<cols;col++){
+            
+                System.out.print("["+row+"]"+"["+col+"]"+" :");
+                myArray[row][col]=s.nextInt();
+              
+            }
+        }
+        System.out.println("Arrays : "+Arrays.deepToString(myArray));
+        System.out.println("---------------------------");
+        
+        
     }
     
 }
