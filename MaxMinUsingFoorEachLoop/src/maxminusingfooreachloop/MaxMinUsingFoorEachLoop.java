548c283b708eef79f8a5243fef3605db7ea59dfc
gsparvej <parvejmd4469@gmail.com>
2025-04-25 12:31:05 +0600

    changed

--- MaxMinUsingFoorEachLoop/src/maxminusingfooreachloop/MaxMinUsingFoorEachLoop.java
+++ MaxMinUsingFoorEachLoop/src/maxminusingfooreachloop/MaxMinUsingFoorEachLoop.java
@@ -14,6 +14,7 @@ public class MaxMinUsingFoorEachLoop {
         System.out.println("Enter Array size");
         int input=s.nextInt();
         
+        String result;
         int maxNumber=0;
         int minNumber=999999999;
         
@@ -21,10 +22,14 @@ public class MaxMinUsingFoorEachLoop {
         
         for(int index=0;index<input;index++){
         
-            System.out.println("Input "+(index+1));
+            System.out.println("Input "+(index+1)+position(index+1));
             int userInput=s.nextInt();
             myNumbers[index]=userInput;
         }
+        int index = 0;
+        
+        
+       
         System.out.println("My Numbers :"+Arrays.toString(myNumbers));
         
         for(int number : myNumbers){
@@ -43,6 +48,24 @@ public class MaxMinUsingFoorEachLoop {
         }
         System.out.println("Max Number is :"+maxNumber);
         System.out.println("Min Number is :"+minNumber);
+        
+    }
+    
+    public static String position(int number){
+    
+         switch (number) {
+            case 1: 
+            return "st";
+                case 2: 
+             return "nd";
+                
+                case 3: 
+             return"rd";
+                
+                
+            default:
+                return"th";
+        }
         
     }
     
