a5dcc8fd9e3f0f2783e5c5fc998f7973bf8623ff
gsparvej <parvejmd4469@gmail.com>
2025-04-25 21:20:10 +0600

    add

--- RandomMathGuessNumber/src/randommathguessnumber/RandomMathGuessNumber.java
+++ RandomMathGuessNumber/src/randommathguessnumber/RandomMathGuessNumber.java
@@ -1,12 +1,28 @@
 
 package randommathguessnumber;
 
+import java.util.Scanner;
+
 
 public class RandomMathGuessNumber {
 
     
     public static void main(String[] args) {
         
+        Scanner s=new Scanner(System.in);
+        System.out.print("Guess a Number : ");
+        int a=s.nextInt();
+        int random=(int) (Math.random()*10);
+        System.out.println("Random Number : "+random);
+        
+        if(a==random){
+        
+            System.out.println("You Are Win ");
+        }
+        else{
+        
+            System.out.println("Better Luck Next Time ");
+        }
         
     }
     
