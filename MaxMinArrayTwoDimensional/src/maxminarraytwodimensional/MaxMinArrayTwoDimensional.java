1bb7824a728c3b62d7feeb5dd41e378c193664f1
gsparvej <parvejmd4469@gmail.com>
2025-04-25 11:49:39 +0600

    add

--- MaxMinArrayTwoDimensional/src/maxminarraytwodimensional/MaxMinArrayTwoDimensional.java
+++ MaxMinArrayTwoDimensional/src/maxminarraytwodimensional/MaxMinArrayTwoDimensional.java
@@ -33,7 +33,16 @@ public class MaxMinArrayTwoDimensional {
         System.out.println("Arrays : "+Arrays.deepToString(myArray));
         System.out.println("---------------------------");
         
+        for(int i=0;i<myArray.length;i++){
         
+            for(int j=0;j<myArray[i].length;j++){
+            
+                System.out.print(""+myArray[i][j]+" ");
+            }
+            System.out.println("\n");
+        
+        
+        }
     }
     
 }
