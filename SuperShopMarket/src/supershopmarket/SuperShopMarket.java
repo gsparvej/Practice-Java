1607151b5115fb09a8f4b003c849912c9e5daf97
gsparvej <parvejmd4469@gmail.com>
2025-04-30 02:02:56 +0600

    add

--- SuperShopMarket/src/supershopmarket/SuperShopMarket.java
+++ SuperShopMarket/src/supershopmarket/SuperShopMarket.java
@@ -16,12 +16,15 @@ public class SuperShopMarket {
         g.setgPrice(200+500+1500+200+1500);
         v.setvPrice(200+200+500);
         
-        System.out.println("Grocery Total Price : "+g.getSalePrice());
-        System.out.println("Vegetables Total Price : "+v.getSalePrice());
+        System.out.println(""+g.getSalePrice());
+        System.out.println(""+v.getSalePrice());
         
-        System.out.println(""+g.getTotalPrice());
-        System.out.println("---------------");
-        System.out.println(""+v.getTotalsalePrice());
+        double gro=g.getSalePrice();
+        double veg=v.getSalePrice();
+        double total=gro+veg;
+      
+        System.out.println("--------------");
+        System.out.println("Total : "+total);
     }
     
 }
