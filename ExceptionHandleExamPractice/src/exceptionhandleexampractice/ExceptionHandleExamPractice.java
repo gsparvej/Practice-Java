46ed32c8ac7f09f94273fb842a41cc03922bce96
gsparvej <parvejmd4469@gmail.com>
2025-05-05 09:33:28 +0600

    add

--- ExceptionHandleExamPractice/src/exceptionhandleexampractice/ExceptionHandleExamPractice.java
+++ ExceptionHandleExamPractice/src/exceptionhandleexampractice/ExceptionHandleExamPractice.java
@@ -1,13 +1,30 @@
 
 package exceptionhandleexampractice;
 
+import java.util.Scanner;
+
 
 public class ExceptionHandleExamPractice {
 
    
     public static void main(String[] args) {
         
-        
+//        
+//         try {
+//           
+//            int result = 10 / 0;  
+//            System.out.println("Result: " + result);
+//        } catch (ArithmeticException e) {
+//            
+//            System.out.println("Error: Division by zero is not allowed.");
+//        }
+//
+//        System.out.println("Program continues after the try-catch block.");
+
+
+
+        int result=10/0;
+        System.out.println("Result : "+result);
         
     }
     
