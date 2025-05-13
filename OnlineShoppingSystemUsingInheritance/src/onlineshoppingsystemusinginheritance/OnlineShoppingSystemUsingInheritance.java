fcae31a0d014502a7670455f51feb04b19ef06fd
gsparvej <parvejmd4469@gmail.com>
2025-05-13 13:42:51 +0600

    add

--- OnlineShoppingSystemUsingInheritance/src/onlineshoppingsystemusinginheritance/OnlineShoppingSystemUsingInheritance.java
+++ OnlineShoppingSystemUsingInheritance/src/onlineshoppingsystemusinginheritance/OnlineShoppingSystemUsingInheritance.java
@@ -12,20 +12,37 @@ public class OnlineShoppingSystemUsingInheritance {
 
     public static void main(String[] args) {
         
-        Product p1=new Clothing();
-        Product p2=new Electronics();
-      //  Product p3=new FoodItem( "Chocolates", 1200, "co090");
-      
-      p1.setProductName("Shirt");
-      p1.setProductPrice(900);
-      p1.setProductId("s-0012");
-      
-      p2.setProductName("AirConditioner");
-      p2.setProductPrice(55000);
-      p2.setProductId("AC\00123");
-      
-      
-        Product[] items = { p1, p2};
+//        Product p1=new Clothing();
+//        Product p2=new Electronics();
+//      //  Product p3=new FoodItem( "Chocolates", 1200, "co090");
+//      
+//      p1.setProductName("Shirt");
+//      p1.setProductPrice(900);
+//      p1.setProductId("s-0012");
+//      
+//      
+//      p2.setProductName("AirConditioner");
+//      p2.setProductPrice(55000);
+//      p2.setProductId("AC\00123");
+//      
+//      
+//        Product[] items = { p1, p2};
+//        Cart myCart = new Cart(items);
+        
+           Clothing c=new Clothing();
+           Electronics e=new Electronics();
+           
+           c.setClothSize("XL");
+           c.setProductName("Shirt");
+           c.setProductId("S-001");
+           c.setProductPrice(900);
+           
+           e.setProductName("Air Conditioner");
+           e.setProductPrice(55000);
+           e.setProductId("AC-9709");
+           e.setWarranty("10 Years");
+
+           Product[] items = {c,e};
         Cart myCart = new Cart(items);
         
         myCart.displayDetails();
